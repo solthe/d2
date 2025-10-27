3ea326fc96eb8004b8bb5cb4b09b37f52ad0895c
Sol Choi <69390011+solthe@users.noreply.github.com>
2025-10-27 07:23:08 +0000

    mouse events step 2

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,45 @@ document.body.innerHTML = `
   <canvas id = "canvas" width= "256" height = "256"></canvas>
 `;
 
+<<<<<<< HEAD
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 ctx.strokeStyle = "black";
 ctx.lineWidth = 5;
 ctx.strokeRect(0, 0, canvas.width, canvas.height);
+=======
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d")!; // <-- Non-Null Assertion Operator '!'
+
+if (ctx) {
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 5;
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+} else {
+  // Optionally handle the error case if the context isn't available
+  console.error("Could not get 2D rendering context.");
+}
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e: MouseEvent) => {
+  cursor.active = true;
+  cursor.x = e.offsetX; //relative coordinate
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e: MouseEvent) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
