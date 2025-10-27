f6434b24c2c83dbfdeb1fabcf1f81d72446a27d5
Sol Choi <69390011+solthe@users.noreply.github.com>
2025-10-27 07:23:08 +0000

    canvas step 1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+// import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1>This is a Sketch Pad of Sol~ </h1>
+  <canvas id = "canvas" width= "256" height = "256"></canvas>
 `;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.strokeRect(0, 0, canvas.width, canvas.height);
